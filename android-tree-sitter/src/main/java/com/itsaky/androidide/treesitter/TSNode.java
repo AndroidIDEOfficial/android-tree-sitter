393aed9b018a8e92abdfc29802b8f8fdb376779f
Akash Yadav <itsaky01@gmail.com>
2022-09-29 22:03:09 +0530

    Moved node-related native methods to TSNode class

--- android-tree-sitter/src/main/java/com/itsaky/androidide/treesitter/TSNode.java
+++ android-tree-sitter/src/main/java/com/itsaky/androidide/treesitter/TSNode.java
@@ -15,17 +15,13 @@ public class TSNode {
 
   public TSNode() {}
 
-  public TSNode getParent() {
-    return TreeSitter.nodeParent(this);
-  }
-
   public TSNode getChild(int index) {
     final var count = getChildCount();
     if (index < 0 || index >= count) {
       throw new IndexOutOfBoundsException("count=" + count + ", index=" + index);
     }
 
-    return TreeSitter.nodeChild(this, index);
+    return getChildAt(index);
   }
 
   public TSNode getNamedChild(int index) {
@@ -34,68 +30,12 @@ public class TSNode {
       throw new IndexOutOfBoundsException("count=" + count + ", index=" + index);
     }
 
-    return TreeSitter.nodeNamedChild(this, index);
+    return getNamedChildAt(index);
   }
 
   public TSNode getChildByFieldName(String fieldName) {
     final var bytes = fieldName.getBytes(StandardCharsets.UTF_8);
-    return TreeSitter.getChildByFieldName(this, bytes, bytes.length);
-  }
-
-  public int getChildCount() {
-    return TreeSitter.nodeChildCount(this);
-  }
-
-  public int getNamedChildCount() {
-    return TreeSitter.nodeNamedChildCount(this);
-  }
-
-  public int getEndByte() {
-    return TreeSitter.nodeEndByte(this);
-  }
-
-  public String getNodeString() {
-    return TreeSitter.nodeString(this);
-  }
-
-  public int getStartByte() {
-    return TreeSitter.nodeStartByte(this);
-  }
-
-  public TSPoint getStartPoint() {
-    return TreeSitter.nodeStartPoint(this);
-  }
-
-  public TSPoint getEndPoint() {
-    return TreeSitter.nodeEndPoint(this);
-  }
-
-  public String getType() {
-    return TreeSitter.nodeType(this);
-  }
-
-  public boolean isNull() {
-    return TreeSitter.nodeIsNull(this);
-  }
-
-  public boolean isNamed() {
-    return TreeSitter.nodeIsNamed(this);
-  }
-
-  public boolean isExtra() {
-    return TreeSitter.nodeIsExtra(this);
-  }
-
-  public boolean isMissing() {
-    return TreeSitter.nodeIsMissing(this);
-  }
-
-  public boolean hasChanges() {
-    return TreeSitter.nodeHasChanges(this);
-  }
-
-  public boolean hasErrors() {
-    return TreeSitter.nodeHasError(this);
+    return getChildByFieldName(bytes, bytes.length);
   }
 
   public TSTreeCursor walk() {
@@ -117,7 +57,8 @@ public class TSNode {
     return null;
   }
 
-  public List<TSNode> findChildrenWithType(final String type, final boolean reverseSearch, final boolean namedOnly) {
+  public List<TSNode> findChildrenWithType(
+      final String type, final boolean reverseSearch, final boolean namedOnly) {
     if (reverseSearch) {
       return findChildrenWithTypeReverse(type, namedOnly);
     }
@@ -157,4 +98,115 @@ public class TSNode {
   public String toString() {
     return "TSNode{" + "id=" + id + ", type=" + (isNull() ? "<null>" : getType()) + '}';
   }
+
+  /**
+   * Get the immediate parent node of this node.
+   *
+   * @return The parent node.
+   */
+  public native TSNode getParent();
+
+  /**
+   * Get the child of the given node at the child index.
+   *
+   * @param index The index of the child.
+   * @return The child at the child index.
+   */
+  private native TSNode getChildAt(int index);
+
+  /**
+   * Get the named child of the given node at the given index.
+   *
+   * @param index The index of the named child.
+   * @return The named child node at the given index.
+   */
+  private native TSNode getNamedChildAt(int index);
+
+  /**
+   * Find the child node of the given node by field name.
+   *
+   * @param bytes The field name of the child.
+   * @param length The length of `fieldName`.
+   * @return The found node.
+   */
+  private native TSNode getChildByFieldName(byte[] bytes, int length);
+
+  /**
+   * Get the number of children of the node.
+   *
+   * @return The number of children.
+   */
+  public native int getChildCount();
+
+  /** Get the number of 'named' child nodes in the node. */
+  public native int getNamedChildCount();
+
+  /**
+   * Get the end byte of this node.
+   *
+   * @return End byte of node.
+   */
+  public native int getEndByte();
+
+  /**
+   * Get the string representation of this node.
+   *
+   * @return The string representation of the node.
+   */
+  public native String getNodeString();
+
+  /**
+   * Get the start byte of this node.
+   *
+   * @return Start byte of node.
+   */
+  public native int getStartByte();
+
+  /**
+   * Get the start position of this node.
+   *
+   * @return The start position.
+   */
+  public native TSPoint getStartPoint();
+
+  /**
+   * Get the end position of this node.
+   *
+   * @return The end position.
+   */
+  public native TSPoint getEndPoint();
+
+  /**
+   * Get the type of this node.
+   *
+   * @return The type of the node.
+   */
+  public native String getType();
+
+  /** Check if the node is null. */
+  public native boolean isNull();
+
+  /**
+   * Check if the node is *named*. Named nodes correspond to named rules in the grammar, whereas
+   * *anonymous* nodes correspond to string literals in the grammar.
+   */
+  public native boolean isNamed();
+
+  /**
+   * Check if the node is *extra*. Extra nodes represent things like comments, which are not
+   * required the grammar, but can appear anywhere.
+   */
+  public native boolean isExtra();
+
+  /**
+   * Check if the node is *missing*. Missing nodes are inserted by the parser in order to recover
+   * from certain kinds of syntax errors.
+   */
+  public native boolean isMissing();
+
+  /** Check if the given node has been edited. */
+  public native boolean hasChanges();
+
+  /** Check if the node is a syntax error or contains any syntax errors. */
+  public native boolean hasErrors();
 }
--- genh.sh
+++ genh.sh
@@ -4,7 +4,7 @@ set -eu
 
 script_dir=$(realpath $(dirname $0))
 
-for header in TreeSitter TSLanguages
+for header in TreeSitter TSLanguages TSNode
 do
   javah -d $script_dir/lib -classpath ${script_dir}/android-tree-sitter/src/main/java com.itsaky.androidide.treesitter.${header}
 done
