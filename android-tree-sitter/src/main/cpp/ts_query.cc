28e39c110303420af7f7faef772da6e142531e1d
Akash Yadav <itsaky01@gmail.com>
2023-06-16 14:25:17 +0530

    fix: add null check for new query instances

--- android-tree-sitter/src/main/cpp/ts_query.cc
+++ android-tree-sitter/src/main/cpp/ts_query.cc
@@ -27,10 +27,13 @@ Java_com_itsaky_androidide_treesitter_TSQuery_00024Native_newQuery(
   const char* c_source;
   uint32_t source_length = env->GetStringLength(source);
   c_source = env->GetStringUTFChars(source, NULL);
-  uint32_t* error_offset = new uint32_t;
-  TSQueryError* error_type = new TSQueryError;
+  auto error_offset = new uint32_t;
+  auto error_type = new TSQueryError;
   TSQuery* query = ts_query_new((TSLanguage*)language, c_source, source_length,
                                 error_offset, error_type);
+  if (query == nullptr) {
+    return 0;
+  }
   fillQuery(env, queryObject, *error_offset, *error_type);
   return (jlong)query;
 }
