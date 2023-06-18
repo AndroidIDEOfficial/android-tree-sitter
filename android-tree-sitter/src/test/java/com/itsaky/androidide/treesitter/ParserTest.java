7ce6b0a7388d838e40e29ae0b9c932ce12e1b5da
Akash Yadav <itsaky01@gmail.com>
2023-06-18 09:52:00 +0530

    tests: update log parser test

--- android-tree-sitter/src/test/java/com/itsaky/androidide/treesitter/ParserTest.java
+++ android-tree-sitter/src/test/java/com/itsaky/androidide/treesitter/ParserTest.java
@@ -169,7 +169,7 @@ public class ParserTest extends TreeSitterTest {
     try (final var parser = new TSParser()) {
       parser.setLanguage(TSLanguageLog.getInstance());
 
-      final var source = "04-19 09:37:12.217  1384  1527 W BroadcastQueue: Background execution not allowed: receiving Intent { act=android.intent.action.USER_PRESENT flg=0x24200010 } to com.google.android.gms/.auth.setup.devicesignals.LockScreenReceiver";
+      final var source = "04-19 09:37:12.217 1384 1527 W BroadcastQueue: Background execution not allowed: receiving Intent { act=android.intent.action.USER_PRESENT flg=0x24200010 } to com.google.android.gms/.auth.setup.devicesignals.LockScreenReceiver";
 
       try (final var tree = parser.parseString(source)) {
         final var rootNode = tree.getRootNode();
