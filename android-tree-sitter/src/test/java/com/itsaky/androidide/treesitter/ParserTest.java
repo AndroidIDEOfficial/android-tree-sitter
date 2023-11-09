45a8134d3da1783308f559bf39623697f26a2143
Akash Yadav <itsaky01@gmail.com>
2023-11-09 19:42:27 +0530

    tests: update tests for kotlin grammar

--- android-tree-sitter/src/test/java/com/itsaky/androidide/treesitter/ParserTest.java
+++ android-tree-sitter/src/test/java/com/itsaky/androidide/treesitter/ParserTest.java
@@ -180,7 +180,7 @@ public class ParserTest extends TreeSitterTest {
         assertThat(rootNode).isNotNull();
         assertThat(rootNode.getChildCount()).isGreaterThan(0);
         assertThat(rootNode.getNodeString()).isEqualTo(
-          "(source_file (class_declaration (type_identifier) (class_body (function_declaration (simple_identifier) (function_value_parameters) (function_body (statements (call_expression (simple_identifier) (call_suffix (value_arguments (value_argument (string_literal)))))))))))");
+          "(source_file (class_declaration name: (type_identifier) body: (class_body (function_declaration name: (simple_identifier) parameters: (function_value_parameters) body: (function_body (statements (call_expression (simple_identifier) (call_suffix (value_arguments (value_argument (string_literal)))))))))))");
       }
     }
   }
